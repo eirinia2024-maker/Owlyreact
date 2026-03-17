d55feaa28dac966818ed820ccbefc719af9cd194
eirinia2024-maker <eirinia2024@gmail.com>
2026-03-17 23:15:04 +0300

    set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Конфиг Vite для GitHub Pages
 export default defineConfig({
-// root можно не указывать, по умолчанию '.'
-base: '/Owlyreact/', // ВАЖНО: имя репозитория c учётом регистра
-});
+  plugins: [react()],
+  base: '/Owlyreact/',
+})
